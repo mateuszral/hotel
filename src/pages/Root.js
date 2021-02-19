43e27381de51a7fdf6382e6ec929416cb94a318f
mateuszral <mateuszral@onet.pl>
2021-02-19 13:25:49 +0100

    Add routes to root file

--- src/pages/Root.js
+++ src/pages/Root.js
@@ -1,7 +1,26 @@
 import React from 'react';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
 
-const Root = () => {
-  return <div>Hello React</div>;
-};
+import MainTemplate from 'templates/MainTemplate';
+
+import { routes } from 'routes';
+
+import Home from 'pages/Home';
+import Facilities from 'pages/Facilities';
+import Rooms from 'pages/Rooms';
+import Contact from 'pages/Contact';
+
+const Root = () => (
+  <BrowserRouter>
+    <MainTemplate>
+      <Switch>
+        <Route exact path={routes.home} component={Home} />
+        <Route exact path={routes.facilities} component={Facilities} />
+        <Route exact path={routes.rooms} component={Rooms} />
+        <Route exact path={routes.contact} component={Contact} />
+      </Switch>
+    </MainTemplate>
+  </BrowserRouter>
+);
 
 export default Root;
