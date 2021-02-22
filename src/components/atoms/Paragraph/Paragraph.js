ef35ce1fce172e044fa1b64a158963a290611b35
mateuszral <mateuszral@onet.pl>
2021-02-22 16:02:15 +0100

    Add paragraph component

---
+++ src/components/atoms/Paragraph/Paragraph.js
@@ -0,0 +1,20 @@
+import styled from 'styled-components';
+
+const Paragraph = styled.p`
+  letter-spacing: 2px;
+  margin: ${({ smallMargin, bigMargin, marginBottom }) => {
+    if (smallMargin) return '5px 0';
+    if (bigMargin) return '40px 0';
+    if (marginBottom) return '0 0 15px';
+    return '15px 0';
+  }};
+  font-weight: ${({ theme, semiBold }) => semiBold && theme.font.weight.semiBold};
+  font-size: ${({ theme, smallFont }) =>
+    smallFont ? theme.font.size.paragraph : theme.font.size.link};
+
+  ${({ theme }) => theme.mq.tablet} {
+    width: ${({ halfWidth }) => halfWidth && '50%'};
+  }
+`;
+
+export default Paragraph;
