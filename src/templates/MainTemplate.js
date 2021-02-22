6fec470730f90f9a33eddd11ae371844229f3e1b
mateuszral <mateuszral@onet.pl>
2021-02-22 12:22:43 +0100

    Update main template

--- src/templates/MainTemplate.js
+++ src/templates/MainTemplate.js
@@ -6,21 +6,49 @@ import { ThemeProvider } from 'styled-components';
 import GlobalStyles from 'assets/styles/globalStyles';
 import { theme } from 'assets/styles/theme';
 
-import Navigation from 'components/Navigation';
+import hero from 'assets/images/hero.png';
+import hero2 from 'assets/images/hero2.png';
+import hero3 from 'assets/images/hero3.png';
 
-const MainTemplate = ({ children }) => (
-  <>
-    <Helmet lang="pl" title="Luxury hotels" />
-    <GlobalStyles />
-    <ThemeProvider theme={theme}>
-      <Navigation />
-      {children}
-    </ThemeProvider>
-  </>
-);
+import Navigation from 'components/Navigation/Navigation';
+import HeroImage from 'components/HeroImage/HeroImage';
+import Testimonials from 'components/Testimonials/Testimonials';
+import Footer from 'components/Footer/Footer';
+
+import { routes } from 'routes';
+
+const MainTemplate = ({ children, location }) => {
+  const { pathname } = location;
+  let image = hero;
+
+  switch (pathname) {
+    case routes.facilities:
+      image = hero2;
+      break;
+    case routes.rooms:
+      image = hero3;
+      break;
+    default:
+      image = hero;
+  }
+
+  return (
+    <>
+      <Helmet lang="en" title="Luxury hotels" />
+      <GlobalStyles />
+      <ThemeProvider theme={theme}>
+        <Navigation />
+        {pathname !== routes.contact && <HeroImage src={image} />}
+        {children}
+        <Testimonials />
+        <Footer />
+      </ThemeProvider>
+    </>
+  );
+};
 
 MainTemplate.propTypes = {
-  children: PropTypes.element.isRequired,
+  children: PropTypes.arrayOf(PropTypes.element.isRequired).isRequired,
 };
 
 export default MainTemplate;
