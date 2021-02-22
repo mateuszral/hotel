9aebb406c936785018f5cfc1f0afdca400282c0b
mateuszral <mateuszral@onet.pl>
2021-02-22 17:21:59 +0100

    Add facilities data

---
+++ src/assets/data/index.js
@@ -0,0 +1,33 @@
+import gym from 'assets/images/gym.png';
+import bar from 'assets/images/bar.png';
+import spa from 'assets/images/spa.png';
+import pool from 'assets/images/pool.png';
+import restaurant from 'assets/images/restaurant.png';
+import laundry from 'assets/images/laundry.png';
+
+export const facilities = [
+  {
+    name: 'THE GYM',
+    image: gym,
+  },
+  {
+    name: 'POOLSIDE BAR',
+    image: bar,
+  },
+  {
+    name: 'THE SPA',
+    image: spa,
+  },
+  {
+    name: 'SWIMMING POOL',
+    image: pool,
+  },
+  {
+    name: 'RESTAURANT',
+    image: restaurant,
+  },
+  {
+    name: 'LAUNDRY',
+    image: laundry,
+  },
+];
